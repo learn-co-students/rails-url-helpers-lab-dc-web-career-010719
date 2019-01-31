aeeb35869455804a546cf0ff3b89f4c3a7a45cd4
Ben Yellin <byellindc@gmail.com>
2019-01-31 15:00:13 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,11 +6,18 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def activate
+    student = Student.find(params[:id])
+    student.update(active: !student.active)
+    redirect_to student
   end
 
   private
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
+  def set_student
+    @student = Student.find(params[:id])
+  end
 end
