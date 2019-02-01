de5874180d105a1f31b358775e4dc314f0506cbf
canikwe <c.anikwe@gmail.com>
2019-02-01 13:30:42 -0500

    worked with helpers and created forms

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   # resources :students, only: [:index, :show]
   # However, for the sake of this lab and seeing how you can pass params through
   # the route, we'll keep it explicit for now and refactor later.
-  get "students/:id", to: "students#show"
+  get "/students/:id", to: "students#show", as: "student"
+  get "/students/:id/edit", to: "students#edit", as: "edit_student"
+  patch "/students/:id", to: "students#update"
+  get "/students/:id/activate_student", to: "students#activate", as: "activate_student"
+  # post "/students", to: "students"
+  # get "/students/:id/update", to: "students#update"
 end
