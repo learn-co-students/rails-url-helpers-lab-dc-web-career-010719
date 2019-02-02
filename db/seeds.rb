0eada31e257b875c99204d5921637d83bc260155
Dusty211 <kylebenz@hotmail.com>
2019-02-02 16:30:00 -0500

    All tests passing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: 'Billy', last_name: 'Rousseau')
+Student.create(first_name: 'Chuck', last_name: 'Norris')
+Student.create(first_name: 'Steven', last_name: 'Seagal')
+Student.create(first_name: 'Arnold', last_name: 'Schwarznegger')
+Student.create(first_name: 'Clint', last_name: 'Eastwood')
+Student.create(first_name: 'Billy', last_name: 'Crystal')
+Student.create(first_name: 'Bruce', last_name: 'Willis')
+Student.create(first_name: 'Norm', last_name: 'McDonald')
+Student.create(first_name: 'Cheech', last_name: 'Maron')
